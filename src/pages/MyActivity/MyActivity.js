8db4d67cd5d6870824f95f66119ba45ff84658c3
thanawin srisuk <6210612724@student.tu.ac.th>
2022-11-19 12:28:05 +0700

    add activity createact page

---
+++ src/pages/MyActivity/MyActivity.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './MyActivity.module.css'
+import NavBar from '../../components/navbar/NavBar';
+import MyActivityCard from '../../components/MyActivityCard/MyActivityCard';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>     
+        <div className={styles.card_container}>
+            <div className={styles.cardbox}>
+               <MyActivityCard/>
+            </div>
+        </div>
+        <NavBar className={styles.navbar}/>
+    </div>
+  )
+}

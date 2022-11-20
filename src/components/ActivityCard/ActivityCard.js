8b63ccfc5f40b9030f147d1e6f335eb34b70b871
boyking <boykingkao@gmail.com>
2022-11-20 20:27:30 +0700

    gohza

--- src/components/ActivityCard/ActivityCard.js
+++ src/components/ActivityCard/ActivityCard.js
@@ -2,6 +2,19 @@ import React from 'react'
 import styles from './ActivityCard.module.css'
 
 
+function Join_activity(pros){
+
+  let activityName = pros.act
+  let user = pros.user
+  alert(`
+ชื่อกิจกรรมที่จะเข้าร่วม : ${activityName}
+ชื่อ username account นี้ : ${user}
+  `)
+
+  fetch(`http://localhost:4000/joinActivity?activityName=${activityName}&guest=${user}`).then(
+
+  )
+}
 
 export default function ActivityCard(pros) {
     const{act,createby,user} = pros
@@ -9,7 +22,7 @@ export default function ActivityCard(pros) {
       <div className={styles.container}>  
         <p className={styles.act_title}>{act}</p>
         <div className={styles.confirm_container}>
-          <button className={styles.confirm_btn}>เข้าร่วมกิจกรรม</button>
+          <button className={styles.confirm_btn} onClick={() => Join_activity(pros)}>เข้าร่วมกิจกรรม</button>
         </div>
       </div>
     )
--- src/pages/MyActivity/MyActivity.js
+++ src/pages/MyActivity/MyActivity.js
@@ -1,19 +1,67 @@
-import React, {useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './MyActivity.module.css'
 import NavBar from '../../components/navbar/NavBar';
 import MyActivityCard from '../../components/MyActivityCard/MyActivityCard';
 
+
+
 export default function Home() {
-  
-    
-    return (
-      <div className={styles.container}>     
-          <div className={styles.card_container}>
-              <div className={styles.cardbox}>
-                <MyActivityCard/>
-              </div>
-          </div>
-          <NavBar className={styles.navbar}/>
+  const [user, setUser] = useState("test");
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    fetch("http://localhost:4000/auth", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      },
+
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.status == 'ok') {
+          setUser(data.decoded.username)
+
+          
+          
+        } else {
+          console.log("กรุณาทำการ login ก่อน")
+
+        }
+
+        fetch(`http://localhost:4000/myActivity?user=${data.decoded.username}`)
+          .then(response => response.json())
+          .then(data => {
+            console.log(data)
+            //response data จาก backend
+          })
+
+      })
+  }, []);
+
+
+  // useEffect(() => {
+
+
+
+  //   fetch(`http://localhost:4000/myActivity?user=${user}`)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //     })
+  // });
+
+
+
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.card_container}>
+        <div className={styles.cardbox}>
+          <MyActivityCard />
+        </div>
       </div>
-    )
+      <NavBar className={styles.navbar} />
+    </div>
+  )
 }
